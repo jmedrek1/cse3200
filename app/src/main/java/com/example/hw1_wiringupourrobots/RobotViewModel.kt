dc468ad43402d1223ff3d3a03237cd758e2aa454
Jack Medrek <jackmedrek@Jacks-MacBook-Pro-2.local>
2023-09-20 14:27:30 -0400

    added from lecture

--- app/src/main/java/com/example/hw1_wiringupourrobots/RobotViewModel.kt
+++ app/src/main/java/com/example/hw1_wiringupourrobots/RobotViewModel.kt
@@ -1,16 +1,12 @@
 package com.example.hw1_wiringupourrobots
 
 import android.util.Log
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 
-private const val TAG = "RobotViewModel"
-class RobotViewModel : ViewModel() {
-    var currentTurn = 0 // not in given code for hw2
-    init {
-        Log.d(TAG, "instance of RobotViewModel created.")
-    }
-    override fun onCleared() {
-        super.onCleared()
-        Log.d(TAG, "instance of RobotViewModel about to be destroyed.")
-    }
+const val TURN_COUNT_KEY = "TURN_COUNT_KEY"
+class RobotViewModel(private val savedStateHandle: SavedStateHandle) : ViewModel() {
+    private var turnCount: Int
+        get() = savedStateHandle.get(TURN_COUNT_KEY) ?: 0
+        set(value) = savedStateHandle.set(TURN_COUNT_KEY, value)
 }
