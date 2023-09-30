600d4ee27d27e8579c1d3febc2f088becf4a957b
Jack Medrek <jackmedrek@Jacks-MacBook-Pro-2.local>
2023-09-30 12:16:19 -0400

    task 2 WIP - need to fix toasts and limit purchase to 1 time

--- app/src/main/java/com/example/hw1_wiringupourrobots/RobotPurchaseActivity.kt
+++ app/src/main/java/com/example/hw1_wiringupourrobots/RobotPurchaseActivity.kt
@@ -9,13 +9,32 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import kotlinx.coroutines.selects.select
+import org.w3c.dom.Text
+import java.util.Random
+
 private const val EXTRA_ROBOT_ENERGY = "com.bignerdranch.android.robot.current_robot_energy"
+
+private val allRewards = mapOf(
+    R.string.reward_a to 1,
+    R.string.reward_b to 2,
+    R.string.reward_c to 3,
+    R.string.reward_d to 3,
+    R.string.reward_e to 4,
+    R.string.reward_f to 4,
+    R.string.reward_g to 7
+)
 
 class RobotPurchaseActivity : AppCompatActivity() {
 
-    private lateinit var reward_button_a : Button
-    private lateinit var reward_button_b : Button
-    private lateinit var reward_button_c : Button
+    private lateinit var reward_button_left : Button
+    private lateinit var reward_button_middle : Button
+    private lateinit var reward_button_right : Button
+
+    private lateinit var reward_cost_left : TextView
+    private lateinit var reward_cost_middle : TextView
+    private lateinit var reward_cost_right : TextView
+
     private lateinit var robot_energy_available : TextView
     private var robot_energy = 0
 
@@ -23,23 +42,46 @@ class RobotPurchaseActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_robot_purchase)
 
-        reward_button_a = findViewById(R.id.buy_reward_a)
-        reward_button_b = findViewById(R.id.buy_reward_b)
-        reward_button_c = findViewById(R.id.buy_reward_c)
+        reward_button_left = findViewById(R.id.buy_reward_left)
+        reward_button_middle = findViewById(R.id.buy_reward_middle)
+        reward_button_right = findViewById(R.id.buy_reward_right)
+        reward_cost_left = findViewById(R.id.reward_left_cost)
+        reward_cost_middle = findViewById(R.id.reward_middle_cost)
+        reward_cost_right = findViewById(R.id.reward_right_cost)
         robot_energy_available = findViewById(R.id.robot_energy_to_spend)
 
-        //robot_energy = 2 // temporary hardcoded for testing
+        // selecting 3 random rewards out of 7 possible
+        val random = Random()
+        val selectedRewards = mutableMapOf<String, Int>()
+
+        while (selectedRewards.size < 3) {
+            val randomReward = allRewards.keys.random()
+
+            if (!selectedRewards.containsKey(getString(randomReward))) {
+                selectedRewards[getString(randomReward)] = allRewards[randomReward] ?: 0
+            }
+        }
+        val sortedRewards = selectedRewards.entries.sortedBy { it.key } // ascending left to right
+
+        reward_button_left.text = sortedRewards[0].key
+        reward_button_middle.text = sortedRewards[1].key
+        reward_button_right.text = sortedRewards[2].key
+
+        reward_cost_left.text = sortedRewards[0].value.toString()
+        reward_cost_middle.text = sortedRewards[1].value.toString()
+        reward_cost_right.text = sortedRewards[2].value.toString()
+
         robot_energy = intent.getIntExtra(EXTRA_ROBOT_ENERGY, 0)
         robot_energy_available.setText(robot_energy.toString())
 
-        reward_button_a.setOnClickListener{view : View ->
-            makePurchase(1)
+        reward_button_left.setOnClickListener{view : View ->
+            makePurchase(sortedRewards[0].value)
         }
-        reward_button_b.setOnClickListener{view : View ->
-            makePurchase(2)
+        reward_button_middle.setOnClickListener{view : View ->
+            makePurchase(sortedRewards[1].value)
         }
-        reward_button_c.setOnClickListener{view : View ->
-            makePurchase(3)
+        reward_button_right.setOnClickListener{view : View ->
+            makePurchase(sortedRewards[2].value)
         }
 
 
@@ -54,6 +96,7 @@ class RobotPurchaseActivity : AppCompatActivity() {
         const val EXTRA_LAST_PURCHASE = "com.example.hw1_wiringupourrobots.LAST_PURCHASE"
         const val EXTRA_UPDATED_ENERGY = "com.example.hw1_wiringupourrobots.UPDATED_ENERGY"
     }
+    //TODO: fix makePurchase to show toast properly for new rewards
     private fun makePurchase(costOfPurchase : Int){
         if (robot_energy >= costOfPurchase){
             val s1 = when {
