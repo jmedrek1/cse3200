7f3d0d1e9347af3d17bd33a87f59bbc2847687dd
Jack Medrek <jackmedrek@Jacks-MacBook-Pro-2.local>
2023-10-02 16:51:57 -0400

    setting up RobotViewModel

--- app/src/main/java/com/example/hw1_wiringupourrobots/RobotViewModel.kt
+++ app/src/main/java/com/example/hw1_wiringupourrobots/RobotViewModel.kt
@@ -5,10 +5,22 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 
 const val TURN_COUNT_KEY = "TURN_COUNT_KEY"
+const val PURCHASES_KEY = "PURCHASES_KEY"
+const val ENERGY_KEY = "ENERGY_KEY"
 
 class RobotViewModel(private val savedStateHandle: SavedStateHandle) : ViewModel() {
-    var currentTurn = 0
+    var purchases: MutableList<String>
+        get() = savedStateHandle.get(PURCHASES_KEY) ?: mutableListOf()
+        set(value) = savedStateHandle.set(PURCHASES_KEY, value)
+    var energy: Int
+        get() = savedStateHandle.get(ENERGY_KEY) ?: 0
+        set(value) = savedStateHandle.set(ENERGY_KEY, value)
     private var turnCount: Int
         get() = savedStateHandle.get(TURN_COUNT_KEY) ?: 0
         set(value) = savedStateHandle.set(TURN_COUNT_KEY, value)
+
+    fun incrementTurnCount() {
+        turnCount++
+        savedStateHandle.set(TURN_COUNT_KEY, turnCount)
+    }
 }
